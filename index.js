70a0082aa3f5cb40a19e7ca0216b1eea40fa4348
paulmunyao <paulmunyao094@gmail.com>
2022-01-03 13:28:13 +0300

    Add toggle effect on development process

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ jQuery(".design").click(function(){
     jQuery("#show").toggle();
     jQuery(".hide-image").toggle();
 });
+
+jQuery(".development").click(function(){
+    jQuery("#onyesha").toggle();
+    jQuery(".ficha-image").toggle();
+});
