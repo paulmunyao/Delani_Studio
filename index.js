4030005fdae39142e343f45c7129702531b3157b
paulmunyao <paulmunyao094@gmail.com>
2022-01-03 16:47:44 +0300

    Toogle effect on services

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-jQuery(".design").click(function(){
-    jQuery("#show").toggle();
-    jQuery(".hide-image").toggle();
+jQuery("#design").click(function(){
+    jQuery(".design-showing").toggle();
+    jQuery(".hide-design-image").toggle();
+})
+
+jQuery("#development").click(function(){
+    jQuery("#development-showing").toggle();
+    jQuery(".hide-development-image").toggle();
 });
 
-jQuery(".development").click(function(){
-    jQuery("#onyesha").toggle();
-    jQuery(".ficha-image").toggle();
+jQuery("#productManagement").click(function(){
+    jQuery("#product-management-showing").toggle();
+    jQuery(".hide-product-management").toggle();
 });
 
-jQuery(".management").click(function(){
-    jQuery("#man").toggle();
-    jQuery(".hid-image").toggle();
-});
+// $(document).ready(function () {
+//   $("#design").click(function () {
+//     $("#design-showing").toggle();
+//     $(".hide-design-image").toggle();
+//   });
+
+//   $("#development").click(function () {
+//     $("#development-showing").toggle();
+//     $(".hide-development-image").toggle();
+//   });
+
+//   $("#productManagement").click(function () {
+//     $("#product-management-showing").toggle();
+//     $(".hide-product-management").toggle();
+//   });
+// });
+
