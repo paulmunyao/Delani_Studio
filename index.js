b6be083408830e2d34205fa358b5c7226363c887
paulmunyao <paulmunyao094@gmail.com>
2022-01-03 13:49:10 +0300

    Add jQuery effect to product management

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ jQuery(".development").click(function(){
     jQuery("#onyesha").toggle();
     jQuery(".ficha-image").toggle();
 });
+
+jQuery(".management").click(function(){
+    jQuery("#man").toggle();
+    jQuery(".hid-image").toggle();
+});
