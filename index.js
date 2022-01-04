22f14fdea5622f2125e898bc65904b0953c20a12
paulmunyao <paulmunyao094@gmail.com>
2022-01-04 05:14:10 +0300

    Add javascript functionality

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ jQuery("#productManagement").click(function(){
     jQuery(".hide-product-management").toggle();
 });
 
-// $(document).ready(function () {
-//   $("#design").click(function () {
-//     $("#design-showing").toggle();
-//     $(".hide-design-image").toggle();
-//   });
-
-//   $("#development").click(function () {
-//     $("#development-showing").toggle();
-//     $(".hide-development-image").toggle();
-//   });
-
-//   $("#productManagement").click(function () {
-//     $("#product-management-showing").toggle();
-//     $(".hide-product-management").toggle();
-//   });
-// });
-
+function validateform(){
+    
+}
